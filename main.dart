8b2cc5564110c8845be789f794ab742872cc3c41
HDlowrider <robotdude101@gmail.com>
2014-02-26 17:01:44 -0500

    Working on landing page, started Dart file

---
+++ main.dart
@@ -0,0 +1,14 @@
+//imports
+import 'dart:html';
+import 'dart:math' show Random;
+//varibles
+List placeholderText = ["will she say yes?","is she the one?" , "is it a boy?" , "should I do my homework?"];
+final random = new Random();
+var randomInt = random.nextInt(4);
+var textbox = querySelector('#inputQuestion');
+
+void main()
+{
+ textbox.attributes['placeholder'] = placeholderText[randomInt];
+           
+}
