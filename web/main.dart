5dc0c582fa49c4af55cd5e0b2be5c74d55b3741a
HDlowrider <robotdude101@gmail.com>
2014-02-28 23:12:31 -0500

    prettyfied things, chaged some structure

--- web/main.dart
+++ web/main.dart
@@ -2,13 +2,13 @@
 import 'dart:html';
 import 'dart:math' show Random;
 //varibles
-List placeholderText = ["will she say yes?","is she the one?" , "is it a boy?" , "should I do my homework?", "is this real life?", "will I pass?", "will I win?", "do you work?", "am I ready?", "should I eat?", "should I workout?", "should I start a band", "should I sleep?", "are we there yet?", "are we done yet?",];
+List placeholderText = ["will she say yes?","is she the one?" , "is it a boy?" , "should I do my homework?", "is this real life?", "will I pass?", "will I win?", "do you work?", "am I ready?", "should I eat?", "should I workout?", "should I start a band?", "should I sleep?", "are we there yet?", "are we done yet?",];
 List answers = ["yes","no","maybe","shake again","try again later"];
 final random = new Random();
 var randomInt = random.nextInt(placeholderText.length);
 var textbox = querySelector('#inputQuestion');
 var infotext = querySelector('#infotextbefore');
-
+var questionText = querySelector('#question');
 void main()
 {
  textbox.attributes['placeholder'] = placeholderText[randomInt];
@@ -23,7 +23,10 @@ void enterPressed(KeyboardEvent e)
   if (e.keyCode == KeyCode.ENTER)
        {
           var question = textbox.value;
-          
-          
+          infotext.style.opacity = "0";
+          textbox.style.display = "none";
+          questionText.text = question;
+          questionText.style.opacity = "1";
+        
        }
 }
